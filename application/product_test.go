8f44f42544dd6e0ad956412a9c0cfc0720316c22
giovaneoliveirati <giovaneolioveira_ti@hotmail.com>
2023-11-12 20:46:44 -0300

    Trabalhando com gomock e mockgen

--- application/product_test.go
+++ application/product_test.go
@@ -18,7 +18,7 @@ func TestProduct_Enable(t *testing.T) {
 
 	product.Price = 0
 	err = product.Enable()
-	require.Equal(t, "the price most be greater than zero to enable the product", err.Error())
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
 
 }
 
